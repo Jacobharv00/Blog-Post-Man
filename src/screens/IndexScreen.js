c7b5d3fe1ade2107971944088b2508e5665ef388
jacob <jacobharv00@gmail.com>
2022-07-21 23:29:59 -0600

    context refactor in progress

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react'
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
+
+import BlogContext from '../context/BlogContext'
+
+const IndexScreen = () => {
+  const { data, addBlogPost } = useContext( BlogContext )
+
+  return (
+    <View>
+      <Text style={ styles.screenTitle }>Index Screen</Text>
+      <Button title='Add A Post' onPress={ addBlogPost } />
+      <FlatList
+        data={ data }
+        keyExtractor={ ( blogPost ) => blogPost.title }
+        renderItem={ ( { item } ) => (
+          <Text style={ styles.blogTitle }>{ item.title }</Text>
+        ) }
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create( {
+  screenTitle: {
+    padding: 10,
+    marginBottom: 20,
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  blogTitle: {
+    padding: 5,
+    fontSize: 20,
+    textAlign: 'center'
+  }
+} )
+
+export default IndexScreen
