f3b93018a98763627e98a60ce9bfadf166ec3d96
jacob <jacobharv00@gmail.com>
2022-07-22 23:32:32 -0600

    feat: add and wire up function to delete blog post

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,21 +1,29 @@
 import React, { useContext } from 'react'
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
+import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Feather } from '@expo/vector-icons'
 
 import { Context } from '../context/BlogContext'
 
 const IndexScreen = () => {
-  const { data, addBlogPost } = useContext( Context )
+  const { state, addBlogPost, deleteBlogPost } = useContext( Context )
 
   return (
     <View>
       <Text style={ styles.screenTitle }>Index Screen</Text>
       <Button title='Add A Post' onPress={ addBlogPost } />
       <FlatList
-        data={ data }
-        keyExtractor={ ( blogPost ) => blogPost.title }
-        renderItem={ ( { item } ) => (
-          <Text style={ styles.blogTitle }>{ item.title }</Text>
-        ) }
+        data={ state }
+        keyExtractor={ ( blogPost ) => blogPost.id }
+        renderItem={ ( { item } ) => {
+          return (
+            <View style={ styles.row }>
+              <Text style={ styles.blogTitle }>{ item.title } - { item.id }</Text>
+              <TouchableOpacity onPress={ () => deleteBlogPost( item.id ) }>
+                <Feather style={ styles.icon } name='trash' />
+              </TouchableOpacity>
+            </View>
+          )
+        } }
       />
     </View>
   )
@@ -32,6 +40,20 @@ const styles = StyleSheet.create( {
     padding: 5,
     fontSize: 20,
     textAlign: 'center'
+  },
+  icon: {
+    padding: 5,
+    fontSize: 24,
+    textAlign: 'center',
+    color: 'red'
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray'
   }
 } )
 
